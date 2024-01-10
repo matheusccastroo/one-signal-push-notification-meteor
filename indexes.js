cd048042eb8e4e4070f9d401157e2082381f6f19
matheusccastro <camargodecastromatheus@gmail.com>
2024-01-10 15:31:59 -0300

    [feat] - split code between server and client and also apply final fixes for metrics collection.

--- indexes.js
+++
@@ -1,8 +0,0 @@
-import { OneSignalPushNotificationMeteorMetricsCollection } from './collection';
-
-Meteor.startup(() => {
-  OneSignalPushNotificationMeteorMetricsCollection.createIndex(
-    { userId: 1, notificationId: 1, read: 1 },
-    { name: 'userIdNotificationIdReadIndex' }
-  );
-});
